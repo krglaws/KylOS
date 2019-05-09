ff5602fea475336e3d3900d2745fee4cd655a771
Kyle Glaws <kyle.glaws@gmail.com>
2019-05-08 23:08:26 -0400

    working on stage1.asm

--- boot/src/stage1.asm
+++ boot/src/stage1.asm
@@ -33,6 +33,28 @@ print_ram:
 	mov ax, bx	; return RAM to ax
 	jmp print_ram	; loop
 
+reset_disk:
+	mov ah, 0
+	mov dl, 0
+	int 0x13
+	jc reset_disk
+
+	mov ax, 0x7e00
+	mov es, ax
+	xor bx, bx
+
+read_disk:
+	mov ah, 0x02
+	mov al, 1
+	mov ch, 1
+	mov cl, 2
+	mov dh, 0
+	mov dl, 0
+	int 0x13
+	jc read_disk
+	
+	jmp 0x07e0:0
+
 done:
 	hlt
 
@@ -40,4 +62,25 @@ greeting db "Welcome to KylOS!", 0x0a, 0x0d, 0x0a, 0x0d, "Available memory (KBs)
 
 times 510 - ($ - $$) db 0
 dw 0xaa55
+
+
+;**************
+;start sector 2
+;**************
+
+mov ah, 0x0e
+mov bx, greeting2
+
+print_greeting2:
+	mov al, [bx]
+	cmp al, 0
+	je done
+	int 0x10
+	add bx, 1
+	jmp print_greeting2
+
+
+greeting2 db 0x0a, 0x0d, "Successfully read sector 2!", 0
+
+
 
